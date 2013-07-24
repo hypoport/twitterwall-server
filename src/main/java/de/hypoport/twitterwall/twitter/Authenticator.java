3a511a7e608493718e3e7f365e1926f1a5d93246
nitram509 <maki@bitkings.de>
2013-07-24 22:56:54 +0200

    implemented simple search to Twitter using twitter4j with Application-Auth-Only

--- src/main/java/de/hypoport/twitterwall/twitter/Authenticator.java
+++ src/main/java/de/hypoport/twitterwall/twitter/Authenticator.java
@@ -1,4 +1,34 @@
 package de.hypoport.twitterwall.twitter;
 
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
+
 public class Authenticator {
+
+  private String consumerKey;
+  private String consumerSecret;
+
+  public List sucheTweets(String searchQuery) throws TwitterException {
+    ConfigurationBuilder builder = new ConfigurationBuilder();
+    builder.setUseSSL(true);
+    builder.setApplicationOnlyAuthEnabled(true);
+    Twitter twitter = new TwitterFactory(builder.build()).getInstance();
+    twitter.setOAuthConsumer(consumerKey, consumerSecret);
+    twitter.getOAuth2Token();
+
+    Query query = new Query(searchQuery);
+    QueryResult result = twitter.search(query);
+
+    return result.getTweets();
+  }
+
+  public void setConsumerKey(String consumerKey) {
+    this.consumerKey = consumerKey;
+  }
+
+  public void setConsumerSecret(String consumerSecret) {
+    this.consumerSecret = consumerSecret;
+  }
 }
--- src/test/java/de/hypoport/twitterwall/twitter/AuthenticatorTest.java
+++ src/test/java/de/hypoport/twitterwall/twitter/AuthenticatorTest.java
@@ -1,6 +1,10 @@
 package de.hypoport.twitterwall.twitter;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import twitter4j.TwitterException;
+
+import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -8,8 +12,28 @@ import static org.fest.assertions.Assertions.assertThat;
 @Test
 public class AuthenticatorTest {
 
+  private Authenticator authenticator;
+
+  @BeforeMethod
+  public void setup() {
+    authenticator = new Authenticator();
+    authenticator.setConsumerKey(ladeAusEnvironmentVariablen("consumerKey"));
+    authenticator.setConsumerSecret(ladeAusEnvironmentVariablen("consumerSecret"));
+  }
+
+  private String ladeAusEnvironmentVariablen(String envName) {
+    String value = System.getenv(envName);
+    if (value == null || value.isEmpty()) {
+      throw new IllegalStateException("Das BearerToken muss als env-var '" + envName + "' gesetzt sein!");
+    }
+    return value;
+  }
+
   @Test
-  public void test() {
-    assertThat(1).isEqualTo(1);
+  public void simple_Suche_mit_Bearer_token_auth() throws TwitterException {
+
+    List html5 = authenticator.sucheTweets("html5");
+
+    assertThat(html5).isNotEmpty();
   }
 }
