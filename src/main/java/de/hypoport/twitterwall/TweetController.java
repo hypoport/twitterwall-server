feecaf65e5c74ab3303d395add8cb81f15dc727e
oliver.otzen@hypoport.de <oliver.otzen@hypoport.de>
2013-08-02 21:48:09 +0200

    Spring in Gradle eingebaut

---
+++ src/main/java/de/hypoport/twitterwall/TweetController.java
@@ -0,0 +1,11 @@
+package de.hypoport.twitterwall;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bozan
+ * Date: 02.08.13
+ * Time: 20:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class TweetController {
+}
---
+++ src/test/java/de/hypoport/twitterwall/TweetSearchServiceServletTest.java
@@ -0,0 +1,11 @@
+package de.hypoport.twitterwall;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: maki
+ * Date: 02.08.13
+ * Time: 18:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class TweetSearchServiceServletTest {
+}
