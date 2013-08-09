254610254c826e55e428008d2ae240a229a1564f
ollio <oliver.otzen@hypoport.de>
2013-08-09 22:13:05 +0200

    new optional parameters since and since_id added to TweetSearchController

--- src/test/java/de/hypoport/twitterwall/twitter/TweetSearchServiceTest.java
+++ src/test/java/de/hypoport/twitterwall/twitter/TweetSearchServiceTest.java
@@ -1,5 +1,6 @@
 package de.hypoport.twitterwall.twitter;
 
+import com.google.common.base.Optional;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import twitter4j.Status;
@@ -33,7 +34,7 @@ public class TweetSearchServiceTest {
   @Test (enabled = false)
   public void simple_Suche_mit_ConsumerKey_und_ConsumerSecret_auth() throws TwitterException {
 
-    List<Status> tweets = tweetSearchService.searchTweets("html5");
+    List<Status> tweets = tweetSearchService.searchTweets("html5", Optional.<String>absent(), Optional.<Long>absent());
 
     assertThat(tweets).isNotEmpty();
   }
