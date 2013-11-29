41ffe91b2a7d75b1499a537c6c4559ce653e3d4d
nitram509 <maki@bitkings.de>
2013-11-29 22:18:10 +0100

    added context path for app config

--- src/main/java/de/hypoport/twitterwall/AppConfigurationServlet.java
+++ src/main/java/de/hypoport/twitterwall/AppConfigurationServlet.java
@@ -12,7 +12,7 @@ public class AppConfigurationServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    String server = req.getServerName() + ":" + req.getServerPort();
+    String server = req.getServerName() + ":" + req.getServerPort() + req.getContextPath();
     String cfg = "appConfiguration.searchUrl = 'http://" + server + "/search';";
 
     resp.setContentType("application/javascript");
