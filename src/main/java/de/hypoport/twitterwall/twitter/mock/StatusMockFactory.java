fc4fe70f19a3cdca1b190402242049ea20378a88
nitram509 <maki@bitkings.de>
2014-01-12 18:01:21 +0100

    fixed typo

--- src/main/java/de/hypoport/twitterwall/twitter/mock/StatusMockFactory.java
+++ src/main/java/de/hypoport/twitterwall/twitter/mock/StatusMockFactory.java
@@ -12,7 +12,7 @@ class StatusMockFactory {
   static {
     status = asList(
         new StatusMock(
-            "blub invasie",
+            "blub invasive",
             "web"
         ),
         new StatusMock(
